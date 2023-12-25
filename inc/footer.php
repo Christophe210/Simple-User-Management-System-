4a5391296a4929842a297539889c67adc4137f07
Christophe Nshimiyimana <ragonartsofficial@gmail.com>
2023-12-25 14:04:43 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,26 @@
+<div class="well card-footer">
+ 
+</div>
+
+
+
+
+  </body>
+
+
+
+
+  <!-- Jquery script -->
+  <script src="assets/jquery.min.js"></script>
+  <script src="assets/bootstrap.min.js"></script>
+  <script src="assets/jquery.dataTables.min.js"></script>
+  <script src="assets/dataTables.bootstrap4.min.js"></script>
+  <script>
+      $(document).ready(function () {
+          $("#flash-msg").delay(7000).fadeOut("slow");
+      });
+      $(document).ready(function() {
+          $('#example').DataTable();
+      } );
+  </script>
+</html>
